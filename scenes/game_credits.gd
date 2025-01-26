b354da258765c0ca104085d7287dfa1871587fff
adrianafitz <adrianafitz@gmail.com>
2025-01-26 16:48:45 -0600

    New Info & Credits Scenes - still need info

--- scenes/game_credits.gd
+++ scenes/game_credits.gd
@@ -15,8 +15,16 @@ func _process(delta: float) -> void:
 
 
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file(MainMenuScene)
+	get_tree().change_scene_to_file(InfoScene)
 
 
 func _on_next_button_pressed() -> void:
-	get_tree().change_scene_to_file(CreditScene)
+	get_tree().change_scene_to_file(MainMenuScene)
+
+
+func _on_ggj_button_pressed() -> void:
+	OS.shell_open("https://globalgamejam.org/")
+
+
+func _on_igdatc_button_pressed() -> void:
+	OS.shell_open("https://igdatc.org/")
