06d5c561f8e5506236a4a9134abb8a5fe04f8b2e
ickmiester <ericmickols@gmail.com>
2025-01-26 15:59:43 -0600

    changing from process to physicsProcess

--- scenes/character.gd
+++ scenes/character.gd
@@ -21,7 +21,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if(Input.is_action_just_pressed("reset")):
 		get_tree().reload_current_scene()
 		pass
