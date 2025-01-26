76fe51521dbe70262c94b549e6ba088c220f4ccf
adrianafitz <adrianafitz@gmail.com>
2025-01-26 17:06:09 -0600

    Info scene

--- scenes/game_credits.gd
+++ scenes/game_credits.gd
@@ -20,11 +20,3 @@ func _on_back_button_pressed() -> void:
 
 func _on_next_button_pressed() -> void:
 	get_tree().change_scene_to_file(MainMenuScene)
-
-
-func _on_ggj_button_pressed() -> void:
-	OS.shell_open("https://globalgamejam.org/")
-
-
-func _on_igdatc_button_pressed() -> void:
-	OS.shell_open("https://igdatc.org/")
